266ff41b44b567ec9b447c3c560afdbfd9b4318e
JuliaIzotova <yuizotova15@gmail.com>
2024-09-18 20:35:01 +0300

    small fix

---
+++ hw_3/task_2.js
@@ -0,0 +1,24 @@
+//Task 2
+
+const num = "5";
+console.log(Number(num) + Number(num + num) + Number(num + num + num));
+
+
+
+//Возможные варианты решения задачи 
+// let number = parseInt(2.9)
+// number = String(number)
+// console.log(Number(number) + Number(number + number) + Number(number + number + number))
+
+// const num = "5";
+// console.log(+num + +(num + num) + +(num + num + num));
+
+// let number = String(5);
+// console.log(Number(number) + Number(number + number) + Number(number + number + number));
+
+// const n = 5;
+// const a1 = String(n);
+// const a2 = String(n) + a1;
+// const a3 = String(n) + a2;
+// console.log(+a1 + +a2 + +a3);
+// console.log(+("" + n) + +("" + n + n) + +("" + n + n + n));
