98bd2544879130448d188c03569d76dde6959c81
JuliaIzotova <yuizotova15@gmail.com>
2024-09-19 14:57:41 +0300

    add hw_4

---
+++ hw_4/task_1.js
@@ -0,0 +1,11 @@
+const salary = 900;
+let grade;
+if (salary >= 1000) {
+    console.log(grade = "middle")
+} else {
+    console.log(grade = "junior")
+}
+
+// попробовала с тернарным оператором
+// const salary1 = 900;
+// console.log (salary1 >= 1000 ? "middle" : "junior")
---
+++ hw_4/task_2.js
@@ -0,0 +1,16 @@
+let minAge = 18;
+let maxAge = 60;
+let age = 17;
+if(typeof(age) !== "number"){
+    console.log("Incorrect data type")
+}else{
+    if(age < minAge){
+        console.log("You don't have access cause your age is " + age + " It's less then")
+    }else if (age >= minAge && age < maxAge) {
+        console.log("Welcome!")
+    }else if (age > maxAge) {
+        console.log("Keep calm and look Culture channel")
+    }else{
+        console.log("Technical work")
+    }
+}
