35be743905f63483269b8955ce4826a13b0b1526
JuliaIzotova <yuizotova15@gmail.com>
2024-11-11 09:03:04 +0300

    clear code

--- hw_14/task_1.ts
+++ hw_14/task_1.ts
@@ -1,8 +1,7 @@
 interface IVehicle {
-    getDetails(): string;  // Метод для получения информации о транспортном средстве
-    start(): string;       // Метод для запуска транспортного средства
+    getDetails(): string;  
+    start(): string;       
   }
-
 abstract class Vehicle implements IVehicle {
     constructor(public make: string, public model: string) {}
     abstract getDetails(): string;
@@ -10,7 +9,6 @@ abstract class Vehicle implements IVehicle {
         return `The vehicle ${this.make} ${this.model} is starting`;
     }
 }
-
 class Car extends Vehicle {
     year: number;
     constructor(make: string, model: string, year: number) {
@@ -21,7 +19,6 @@ class Car extends Vehicle {
         return `${this.make} ${this.model}, ${this.year}`;
     }
 }
-
 const myCar = new Car('Mashina', 'Rabochaya', 1900);
-console.log(myCar.getDetails()); // "Toyota Corolla, 2020"
-console.log(myCar.start()); // "The vehicle Toyota Corolla is starting."
+console.log(myCar.getDetails()); 
+console.log(myCar.start()); 
