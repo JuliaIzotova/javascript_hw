d0689dcfa2230a7bd6afe4e79b36a9b4774b708e
JuliaIzotova <yuizotova15@gmail.com>
2024-11-11 15:59:06 +0300

    add hw_15

---
+++ hw_15/task_1.ts
@@ -0,0 +1,31 @@
+// 1. Создайте дженерик функцию wrapInArray, которая принимает значение любого типа и возвращает его
+// в виде массива этого типа.
+function wrapInArray<T>(arg: T): T[] {
+    return [arg];
+ }
+
+const numberArray = wrapInArray(5); // [5]
+const stringArray = wrapInArray('Hello'); // ['Hello']
+console.log(numberArray);
+console.log(stringArray);
+
+// 2. Создайте дженерик функцию getLastItem, которая принимает массив элементов типа T,
+// и возвращает последний элемент (типа T).
+
+function getLastItem<T>(value: T[]): T {
+    return value[value.length -1];
+}
+
+console.log(getLastItem([1, 2, 3, 4]));
+console.log(getLastItem(['a', 'b', 'c'])); 
+
+//3. Создайте дженерик интерфейс IPair, который принимает два типа T и U и содержит поля first: T и second: U. 
+//Реализуйте функцию, принимающую IPair и возвращающую строку, описывающую пару.
+interface IPair <T, U> {
+    first: T;
+    second: U;
+}
+function describePair<T, U>(pair: IPair<T, U>): string {
+    return `${pair.first} ${pair.second}`;
+}
+console.log(describePair({first: 'Julia', second: 'Izotova'}))
---
+++ hw_15/task_2.ts
@@ -0,0 +1,25 @@
+
+function map<T, U>(arr: T[], callback:(value: T, index: number) => U): U[] {
+  const newArr: U[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i], i));
+  }
+  return newArr;
+}
+console.log(map([1, 2, 3, 4, 5], (value, index) => value * index)); 
+
+//еще через type
+
+type callback1<T, U> = (value: T, index: number) => U;
+function map1<T, U>(array: T[], callback: callback1<T, U>): U[] {
+    const newArr: U[] = [];
+    for(let i=0; i < array.length; i++){
+        newArr.push(callback(array[i], i));
+    }
+    return newArr;
+}
+
+const result = map1([1, 2, 3, 4, 5], (value, index) => value * index);
+console.log(result);
+
+typeof null 
